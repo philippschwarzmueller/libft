e7a76f68b16426319846e89fdb7dc3627248bd3f
Philipp Schwarzmuller <pschwarz@student.42heilbronn.de>
2022-10-12 10:48:20 +0200

    Add memmove unfinished

--- ft_memmove.c
+++ ft_memmove.c
@@ -1,26 +1,17 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   libft.h                                            :+:      :+:    :+:   */
+/*   ft_memmove.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: pschwarz <pschwarz@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/10/11 14:45:59 by pschwarz          #+#    #+#             */
-/*   Updated: 2022/10/12 10:45:57 by pschwarz         ###   ########.fr       */
+/*   Created: 2022/10/12 10:33:49 by pschwarz          #+#    #+#             */
+/*   Updated: 2022/10/12 10:47:20 by pschwarz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef LIBFT_H
-# define LIBFT_H
-
-int		ft_isalpha(int c);
-int		ft_isdigit(int c);
-int		ft_isalnum(int c);
-int		ft_isascii(int c);
-int		ft_isprint(int c);
-void	*ft_memset(void *b, int c, int len);
-void	*ft_bzero(void *s, int n);
-void	*ft_memcpy(void *dst, const void *src, int n);
-void	*ft_memmove(void *dst, const void *src, int len);
-
-#endif
+void	*ft_memmove(void *dst, const void *src, int len)
+{
+	//TODO find what isnt the same as in ft_memcpy
+	return (dst);
+}

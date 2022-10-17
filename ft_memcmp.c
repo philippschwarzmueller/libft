734d685cd44c0b47a4d8d6faacfe605fdbdd0336
Philipp Schwarzmuller <pschwarz@student.42heilbronn.de>
2022-10-17 11:43:49 +0200

    Fix type error and formatting

--- ft_memcmp.c
+++ ft_memcmp.c
@@ -6,25 +6,26 @@
 /*   By: pschwarz <pschwarz@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/14 10:40:38 by pschwarz          #+#    #+#             */
-/*   Updated: 2022/10/14 11:14:48 by pschwarz         ###   ########.fr       */
+/*   Updated: 2022/10/17 11:42:18 by pschwarz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-int	memcmp(const void *s1, const void *s2, size_t n)
+int	ft_memcmp(const void *s1, const void *s2, size_t n)
 {
-	// TODO Fix type error
-	unsigned char	*p1 = s1;
-	unsigned char	*p2 = s2;
+	unsigned char	*p1;
+	unsigned char	*p2;
 
+	p1 = (unsigned char *) s1;
+	p2 = (unsigned char *) s2;
 	if (n != 0)
 	{
 		while (n != 0)
 		{
 			if (p1 != p2)
 			{
-				return (p1 - p2);
+				return (*p1 - *p2);
 			}
 			n--;
 		}

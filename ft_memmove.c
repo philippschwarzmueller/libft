70ef549e348387a1aaec9291b007e7ae70e82449
Philipp Schwarzmuller <pschwarz@student.42heilbronn.de>
2022-10-19 16:15:53 +0200

    Fix memmove

--- ft_memmove.c
+++ ft_memmove.c
@@ -6,12 +6,34 @@
 /*   By: pschwarz <pschwarz@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/12 10:33:49 by pschwarz          #+#    #+#             */
-/*   Updated: 2022/10/12 11:14:18 by pschwarz         ###   ########.fr       */
+/*   Updated: 2022/10/19 15:15:42 by pschwarz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-/* void	*ft_memmove(void *dst, const void *src, unsigned int len)
+void	*ft_memmove(void *dst, const void *src, unsigned int len)
 {
-	//TODO find what isnt the same as in ft_memcpy
+	char	*d;
+	char	*s;
+	char	*d_last;
+	char	*s_last;
+
+	d = dst;
+	s = src;
+	d_last = d + len - 1;
+	s_last = s + len - 1;
+	if (d < s)
+	{
+		while (len--)
+		{
+			*d++ = *s++;
+		}
+	}
+	else
+	{
+		while (len--)
+		{
+			*d_last-- = *s_last--;
+		}
+	}
 	return (dst);
-} */
+}

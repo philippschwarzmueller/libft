4bd01596a9dfc6032e14fe3320ba8be154bbf2fe
Philipp Schwarzmuller <pschwarz@student.42heilbronn.de>
2022-11-03 14:13:27 +0100

    Fix checking for n of 0 and end of src

--- ft_strlcpy.c
+++ ft_strlcpy.c
@@ -6,7 +6,7 @@
 /*   By: pschwarz <pschwarz@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/12 10:59:55 by pschwarz          #+#    #+#             */
-/*   Updated: 2022/11/03 11:50:22 by pschwarz         ###   ########.fr       */
+/*   Updated: 2022/11/03 14:05:08 by pschwarz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,8 +17,11 @@ size_t	ft_strlcpy(char *dst, const char *src, size_t dstsize)
 	unsigned int	i;
 
 	i = 0;
-	// TODO need to check if src[i] exists?
-	while (i < dstsize - 1)
+	if (dstsize == 0)
+	{
+		return (ft_strlen((char *)src));
+	}
+	while (src[i] != '\0' && i < dstsize - 1)
 	{
 		dst[i] = src[i];
 		i++;

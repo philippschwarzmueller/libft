fc6808bcac174bc5b7f8176aaa66e1d0c49700f1
Philipp Schwarzmuller <pschwarz@student.42heilbronn.de>
2022-11-04 11:40:29 +0100

    Fix return type

--- ft_strnstr.c
+++ ft_strnstr.c
@@ -6,7 +6,7 @@
 /*   By: pschwarz <pschwarz@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/14 11:45:32 by pschwarz          #+#    #+#             */
-/*   Updated: 2022/11/04 11:29:08 by pschwarz         ###   ########.fr       */
+/*   Updated: 2022/11/04 11:39:25 by pschwarz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,25 +14,27 @@
 
 char	*ft_strnstr(const char *haystack, const char *needle, size_t len)
 {
-	int	i;
-	int	j;
+	int		i;
+	int		j;
+	char	*res;
 
 	i = 0;
 	j = 0;
+	res = (char *) haystack;
 	if (ft_strlen(needle) == 0)
 	{
-		return (haystack);
+		return (res);
 	}
-	while (haystack[i] != '\0' && len >= ft_strlen(needle))
+	while (res[i] != '\0' && len >= ft_strlen(needle))
 	{
-		while (haystack[i + j] == needle[j]
-			&& haystack[i + j] != '\0')
+		while (res[i + j] == needle[j]
+			&& res[i + j] != '\0')
 		{
 			j++;
 		}
 		if (needle[j] == '\0')
 		{
-			return ((char *)haystack + i);
+			return (res + i);
 		}
 		i++;
 		j = 0;

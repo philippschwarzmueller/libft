18e5e3a0f09e461698dccbb8f0ddb351c6cc302e
Philipp Schwarzmuller <pschwarz@student.42heilbronn.de>
2022-11-04 11:44:06 +0100

    Fix too many lines norminette

--- ft_strnstr.c
+++ ft_strnstr.c
@@ -6,7 +6,7 @@
 /*   By: pschwarz <pschwarz@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/14 11:45:32 by pschwarz          #+#    #+#             */
-/*   Updated: 2022/11/04 11:39:25 by pschwarz         ###   ########.fr       */
+/*   Updated: 2022/11/04 11:42:55 by pschwarz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,15 +27,12 @@ char	*ft_strnstr(const char *haystack, const char *needle, size_t len)
 	}
 	while (res[i] != '\0' && len >= ft_strlen(needle))
 	{
-		while (res[i + j] == needle[j]
-			&& res[i + j] != '\0')
+		while (res[i + j] == needle[j] && res[i + j] != '\0')
 		{
 			j++;
 		}
 		if (needle[j] == '\0')
-		{
 			return (res + i);
-		}
 		i++;
 		j = 0;
 		len--;

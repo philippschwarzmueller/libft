f12bd74e15e24fb8804aead741268f5d8e98c677
Philipp Schwarzmüller <philipp.schwarzmueller@gmail.com>
2022-11-04 22:13:42 +0100

    WIP itoa

--- ft_itoa.c
+++ ft_itoa.c
@@ -3,31 +3,62 @@
 /*                                                        :::      ::::::::   */
 /*   ft_itoa.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: pschwarz <pschwarz@student.42.fr>          +#+  +:+       +#+        */
+/*   By: macbook <macbook@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/18 13:29:10 by pschwarz          #+#    #+#             */
-/*   Updated: 2022/10/22 19:19:40 by pschwarz         ###   ########.fr       */
+/*   Updated: 2022/11/04 22:09:28 by macbook          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
+static int	num_len(int n);
+
 char	*ft_itoa(int n)
 {
 	char	*res;
 	size_t	len;
+	size_t	i;
 
-	len = n;
-	res = malloc(sizeof(char) * len);
-	/* while (n != 0)
+	len = num_len(n);
+	i = 0;
+	res = malloc(sizeof(char) * len + 1);
+	if (!res)
+		return (NULL);
+	res[len - 1] = '\0';
+	if (n < 0)
+		res[i] = '-';
+		i++;
+	while (len > i)
 	{
-		n++;
-	} */
+		if (n < 0)
+		{
+			res[len] = '0' + n % 10 * (-1);
+			n = n / 10;
+		}
+		else
+		{
+			res[len] = '0' + n % 10;
+			n = n / 10;
+		}
+		len--;
+	}
 	return (res);
 }
-// TODO FIX THIS!!!!!
-/*
-alloc correct length
-go front to back with putting the result of n % 10 at back of res
-stop going front to back once 0 % 10 is reached
-*/
+
+static int	num_len(int n)
+{
+	int	len;
+
+	len = 0;
+	if (n >= 0 && n < 10)
+		return (1);
+	if (n < 0)
+		len++;
+	while (n)
+	{
+		n /= 10;
+		len++;
+	}
+	return (len);
+}

f7e86b063cf7fa5e32de940c387e75d31e54babe
Philipp Schwarzmuller <pschwarz@student.42heilbronn.de>
2022-11-06 14:35:28 +0100

    Fix for len 0 and count last element too

--- ft_lstsize.c
+++ ft_lstsize.c
@@ -6,7 +6,7 @@
 /*   By: pschwarz <pschwarz@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/19 10:18:11 by pschwarz          #+#    #+#             */
-/*   Updated: 2022/10/19 13:33:46 by pschwarz         ###   ########.fr       */
+/*   Updated: 2022/11/06 14:34:10 by pschwarz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,10 +17,14 @@ int	ft_lstsize(t_list *lst)
 	int	i;
 
 	i = 0;
-	while (lst->next)
+	if (!lst)
+		return (0);
+	while (lst->next != NULL)
 	{
 		lst = lst->next;
 		i++;
 	}
+	if (lst->next == NULL)
+		i++;
 	return (i);
 }

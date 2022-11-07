f4a772752d16f1d021614a43d7d18145c5eea925
Philipp Schwarzmuller <pschwarz@student.42heilbronn.de>
2022-11-07 17:36:20 +0100

    Fix freeing memory on calloc fail

--- ft_split.c
+++ ft_split.c
@@ -6,33 +6,37 @@
 /*   By: pschwarz <pschwarz@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/18 10:02:23 by pschwarz          #+#    #+#             */
-/*   Updated: 2022/11/07 13:23:23 by pschwarz         ###   ########.fr       */
+/*   Updated: 2022/11/07 17:28:00 by pschwarz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-static int	count_splits(char const *s, char c);
+static int	count_strings(char const *s, char c);
+static char	**free_allocation(char **res, int curr_start);
 
 char	**ft_split(char const *s, char c)
 {
 	char	**res;
 	int		i;
 	int		curr_start;
+	char	*temp;
 
-	i = 0;
 	curr_start = 0;
-	res = (char **)malloc(sizeof(char *) * (count_splits(s, c) + 1));
+	res = malloc(sizeof(char *) * (count_strings(s, c) + 1));
 	if (!res)
 		return (NULL);
-	while (*s != 0)
+	while (*s != '\0')
 	{
 		i = 0;
 		while (s[i] != c && s[i] != '\0')
 			i++;
 		if (i)
 		{
-			res[curr_start++] = ft_memcpy(ft_calloc(i + 1, sizeof(char)), s, i);
+			temp = ft_calloc(i + 1, sizeof(char));
+			if (!temp)
+				return (free_allocation(res, curr_start));
+			res[curr_start++] = ft_memcpy(temp, s, i);
 			s = s + i;
 		}
 		else
@@ -42,7 +46,7 @@ char	**ft_split(char const *s, char c)
 	return (res);
 }
 
-static int	count_splits(char const *s, char c)
+static int	count_strings(char const *s, char c)
 {
 	int	res;
 	int	i;
@@ -63,4 +67,16 @@ static int	count_splits(char const *s, char c)
 		}
 	}
 	return (res);
+}
+
+static char	**free_allocation(char **res, int curr_start)
+{
+	curr_start--;
+	while (curr_start >= 0)
+	{
+		free(res[curr_start]);
+		curr_start--;
+	}
+	free(res);
+	return (NULL);
 }

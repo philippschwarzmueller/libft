e1c5b3ad3136930b85cbfd8eb34544e3ec22b7c1
Philipp Schwarzmuller <pschwarz@student.42heilbronn.de>
2022-11-07 17:48:16 +0100

    Fix norm error of function length > 25 lines

--- ft_split.c
+++ ft_split.c
@@ -6,43 +6,24 @@
 /*   By: pschwarz <pschwarz@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/18 10:02:23 by pschwarz          #+#    #+#             */
-/*   Updated: 2022/11/07 17:28:00 by pschwarz         ###   ########.fr       */
+/*   Updated: 2022/11/07 17:47:20 by pschwarz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
 static int	count_strings(char const *s, char c);
+static char	**write_strings(char const *s, char c, char **res);
 static char	**free_allocation(char **res, int curr_start);
 
 char	**ft_split(char const *s, char c)
 {
 	char	**res;
-	int		i;
-	int		curr_start;
-	char	*temp;
 
-	curr_start = 0;
 	res = malloc(sizeof(char *) * (count_strings(s, c) + 1));
 	if (!res)
 		return (NULL);
-	while (*s != '\0')
-	{
-		i = 0;
-		while (s[i] != c && s[i] != '\0')
-			i++;
-		if (i)
-		{
-			temp = ft_calloc(i + 1, sizeof(char));
-			if (!temp)
-				return (free_allocation(res, curr_start));
-			res[curr_start++] = ft_memcpy(temp, s, i);
-			s = s + i;
-		}
-		else
-			s++;
-	}
-	res[curr_start] = NULL;
+	res = write_strings(s, c, res);
 	return (res);
 }
 
@@ -66,6 +47,33 @@ static int	count_strings(char const *s, char c)
 			}
 		}
 	}
+	return (res);
+}
+
+static char	**write_strings(char const *s, char c, char **res)
+{
+	char	*temp;
+	int		i;
+	int		curr_start;
+
+	curr_start = 0;
+	while (*s != '\0')
+	{
+		i = 0;
+		while (s[i] != c && s[i] != '\0')
+			i++;
+		if (i)
+		{
+			temp = ft_calloc(i + 1, sizeof(char));
+			if (!temp)
+				return (free_allocation(res, curr_start));
+			res[curr_start++] = ft_memcpy(temp, s, i);
+			s = s + i;
+		}
+		else
+			s++;
+	}
+	res[curr_start] = NULL;
 	return (res);
 }
 

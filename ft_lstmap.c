cd8fc942ac4836c5a94e040ae803d72ed8195572
Philipp Schwarzmuller <pschwarz@student.42heilbronn.de>
2022-11-07 17:34:31 +0100

    Fix protection on lstnew fail

--- ft_lstmap.c
+++ ft_lstmap.c
@@ -6,7 +6,7 @@
 /*   By: pschwarz <pschwarz@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/06 15:41:31 by pschwarz          #+#    #+#             */
-/*   Updated: 2022/11/06 15:56:18 by pschwarz         ###   ########.fr       */
+/*   Updated: 2022/11/07 15:16:40 by pschwarz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,7 +23,8 @@ t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
 		new_node = ft_lstnew(f(lst->content));
 		if (!new_node)
 		{
-			ft_lstclear(&new_node, del);
+			ft_lstclear(&res, del);
+			del(lst->content);
 			return (NULL);
 		}
 		ft_lstadd_back(&res, new_node);

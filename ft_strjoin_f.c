e0852046e199a4e4125e187980db7d3617c4250b
Philipp Schwarzmuller <pschwarz@student.42heilbronn.de>
2023-02-27 16:20:48 +0100

    add oph inspired strjoin alternative with free

---
+++ ft_strjoin_f.c
@@ -0,0 +1,41 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strjoin_f.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: pschwarz <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/02/27 16:15:08 by pschwarz          #+#    #+#             */
+/*   Updated: 2023/02/27 16:17:59 by pschwarz         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_strjoin_f(char *s1, char *s2)
+{
+	char	*res;
+	size_t	i;
+	size_t	len;
+	size_t	len_s1;
+	size_t	j;
+
+	if (!s1 || !s2)
+		return (NULL);
+	len_s1 = ft_strlen(s1);
+	len = len_s1 + ft_strlen(s2);
+	res = malloc(len * sizeof(char));
+	if (!res)
+		return (NULL);
+	i = 0;
+	j = 0;
+	while (i < len)
+	{
+		if (i < len_s1)
+			res[i] = s1[i];
+		else
+			res[i] = s2[j++];
+		i++;
+	}
+	return (free(s1), res);
+}
